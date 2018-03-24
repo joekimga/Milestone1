1769101c2ad76d0e483e662ac72e2f53ee611e0a
Joseph Kim <gtburneraddy@gmail.com>
2018-03-24 19:06:10 -0400

    beginning skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA_DT60r98EojCCROWjrgfoSW6DbLSPM_o",
+    authDomain: "feb2018cbc.firebaseapp.com",
+    databaseURL: "https://feb2018cbc.firebaseio.com",
+    projectId: "feb2018cbc",
+    storageBucket: "feb2018cbc.appspot.com",
+    messagingSenderId: "82644971069"
+  };
+  firebase.initializeApp(config);
+
+  

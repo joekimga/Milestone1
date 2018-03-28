98612dc84d76e6be6d9e1f59a1da62a6978fde9a
Joseph Kim <gtburneraddy@gmail.com>
2018-03-28 08:20:35 -0400

    login button style width 100%

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -22,4 +22,37 @@ window.onclick = function(event) {
     messagingSenderId: "82644971069"
   };
   firebase.initializeApp(config);
-  
+
+
+///////////////////////////////////////////////
+//firebase
+
+
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+  } else {
+    // No user is signed in.
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+  }
+});
+//(TAKE OUT ALERT!!!) 
+//Firebase login 
+  function login() {
+  	//TESTING//////////////////////////////
+  	var userEmail = document.getElementById("email_field").value;
+  	var userPass = document.getElementById("password_field").value;
+  	//window.alert(userEmail + " " + userPass);
+  	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+  		// ...
+  		window.alert("Error : " + errorMessage);
+});
+  }

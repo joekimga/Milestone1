9281c1f712e67670301063318bcb3913056353f8
Sarah Shelden <sarah.shelden113@gmail.com>
2018-03-28 15:19:05 -0400

    google font cdn added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,17 @@
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyA_DT60r98EojCCROWjrgfoSW6DbLSPM_o",
-    authDomain: "feb2018cbc.firebaseapp.com",
-    databaseURL: "https://feb2018cbc.firebaseio.com",
-    projectId: "feb2018cbc",
-    storageBucket: "feb2018cbc.appspot.com",
-    messagingSenderId: "82644971069"
+      apiKey: "AIzaSyA_DT60r98EojCCROWjrgfoSW6DbLSPM_o",
+      authDomain: "feb2018cbc.firebaseapp.com",
+      databaseURL: "https://feb2018cbc.firebaseio.com",
+      projectId: "feb2018cbc",
+      storageBucket: "feb2018cbc.appspot.com",
+      messagingSenderId: "82644971069"
   };
   firebase.initializeApp(config);
 
-  
+//AJAX
+  $.ajax({
+      url: queryURL,
+      method: "GET", API_KEY:"p4UY2y5GONsPOEJh-SfSOHFtq1hqy7n4pNfQh4pkpGBHAn2Vi_cks9rHO_yO2c20i8FbKrFUh8qQcx_Z3fCTqrdiGYhOvnV6SEgANAmWKNQwKoOzUBGy8BHAM8m7WnYx"
+  }).then(updatePage);
+  });

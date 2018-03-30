fc000251a8c1435e7759cb1c4d75539aaeffbf87
Sarah Shelden <sarah.shelden113@gmail.com>
2018-03-29 21:03:20 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,58 @@
-  // Initialize Firebase
-  var config = {
-      apiKey: "AIzaSyA_DT60r98EojCCROWjrgfoSW6DbLSPM_o",
-      authDomain: "feb2018cbc.firebaseapp.com",
-      databaseURL: "https://feb2018cbc.firebaseio.com",
-      projectId: "feb2018cbc",
-      storageBucket: "feb2018cbc.appspot.com",
-      messagingSenderId: "82644971069"
-  };
-  firebase.initializeApp(config);
+  // // Initialize Firebase
+  // var config = {
+  //     apiKey: "AIzaSyA_DT60r98EojCCROWjrgfoSW6DbLSPM_o",
+  //     authDomain: "feb2018cbc.firebaseapp.com",
+  //     databaseURL: "https://feb2018cbc.firebaseio.com",
+  //     projectId: "feb2018cbc",
+  //     storageBucket: "feb2018cbc.appspot.com",
+  //     messagingSenderId: "82644971069"
+  // };
+  // firebase.initializeApp(config);
+
+
+  $(document).on("click", "#trial",function () {
+
+    var trial = $(this).attr("data-trial")
+      console.log("data-trial")
+
+  });
+
+
+
 
 //AJAX
+//   $.ajax({
+//       url: `https://api.yelp.com/v3/businesses/search?term=$snapfitness&location&categories&apikey`,
+//       method: "GET",
+//     // header:{'Authorization': 'Bearer API_KEY:"p4UY2y5GONsPOEJh-SfSOHFtq1hqy7n4pNfQh4pkpGBHAn2Vi_cks9rHO_yO2c20i8FbKrFUh8qQcx_Z3fCTqrdiGYhOvnV6SEgANAmWKNQwKoOzUBGy8BHAM8m7WnYx/"
+//   }).then((res) => {
+//       console.log(res);
+//   }).catch((err) => {
+//       if (err) {
+//           console.log(err);
+//       }
+//   })
+
   $.ajax({
-      url: queryURL,
-      method: "GET", API_KEY:"p4UY2y5GONsPOEJh-SfSOHFtq1hqy7n4pNfQh4pkpGBHAn2Vi_cks9rHO_yO2c20i8FbKrFUh8qQcx_Z3fCTqrdiGYhOvnV6SEgANAmWKNQwKoOzUBGy8BHAM8m7WnYx"
-  }).then(updatePage);
+      url: "https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972",
+      method: "get"
+  }).done(function(response) {
+      console.log(response);
   });
+
+
+      // .then(updatePag)
+
+  // });
+
 
 
 // psuedo code
 
+  //var fit = "";
+  //var welness= "";
+
+  //var search = $(this).attr("data-selfCare");
   //1. will need variables on fit, wellness, retreats, spa, ect
   //2, will need on click functions to access the diffrent part
   //3. the yelp api will need to be connected to get zip code info and then supply info on keywords in proximal area

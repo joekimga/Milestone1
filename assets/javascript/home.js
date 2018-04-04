2febb3d97859d8c1b0a668e1075156acbcce67fb
Sarah Shelden <sarah.shelden113@gmail.com>
2018-04-03 21:37:32 -0400

    functioning api no design

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -22,11 +22,11 @@ $(document).on("click",".option", function () {
     console.log(userChoice)
     });
 
+let userChoiceText;
 
 
-
-$(document).on("click",".option", function () {
-let userChoiceText= $(this).text().trim();
+$(document).on("click",".option", function() {
+userChoiceText= $(this).text().trim();
 console.log(userChoiceText);
     if (userChoiceText === "Fitness"){
         $("#topicTitle").text(`Hi! Great, you have chosen ${userChoiceText}`);
@@ -45,14 +45,13 @@ console.log(userChoiceText);
 
 // zip code config to api
 
-$(document).on("click","#zipCode2", function () {
-
-//APIS
-
+$(document).on("click","#zipButton", function () {
+    var zip = $("#ZipCode2").val().trim();
     $.ajax({
-        url: `  https://cors-anywhere.herokuapp.com/https:\/\/maps.googleapis.com/maps/api/place/textsearch/json?query=gyms+food+spas+retreats+welness+movies+in+&key=AIzaSyDOIrdjOme0yAiuQS8QlE9JTucD7rG81X8`,
+        url: `https://cors-anywhere.herokuapp.com/https:\/\/maps.googleapis.com/maps/api/place/textsearch/json?query=${userChoiceText}+in+Atlanta+${zip}&key=AIzaSyDOIrdjOme0yAiuQS8QlE9JTucD7rG81X8`,
         method: "GET",
     }).done(function(response) {
+        $("#whateverDiv").text(response.results[0].name);
         console.log(response);
     });
 
@@ -67,9 +66,29 @@ $(document).on("click","#zipCode2", function () {
     }).done(function(response) {
         console.log(response);
     });
+    //
+    // //APIS
+    //
+    // $.ajax({
+    //     url: `https://cors-anywhere.herokuapp.com/https:\/\/maps.googleapis.com/maps/api/place/textsearch/json?query=gyms+food+spas+retreats+welness+movies+in+Atlanta&key=AIzaSyDOIrdjOme0yAiuQS8QlE9JTucD7rG81X8`,
+    //     method: "GET",
+    // }).done(function(response) {
+    //     console.log(response);
+    // });
+    //
+    // var movie = "The Social Network";
+    // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    //
+    //
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET",
+    // }).done(function(response) {
+    //     console.log(response);
+    // });
+    //
+
 });
-
-
 
 
 

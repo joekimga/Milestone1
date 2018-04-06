d3ea8cb05cdb475522604cdb36543b00aa6720b5
Joseph Kim <gtburneraddy@gmail.com>
2018-04-06 19:57:53 -0400

    login changes

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,7 +1,4 @@
-    // START CODING BELOW!!
-
-    // Initialize Firebase
-   var config = {
+  var config = {
     apiKey: "AIzaSyBrtH3V54Agw02jUBKMTr-F61xW3OLg3og",
     authDomain: "milestoneloginjs.firebaseapp.com",
     databaseURL: "https://milestoneloginjs.firebaseio.com",
@@ -9,54 +6,163 @@
     storageBucket: "milestoneloginjs.appspot.com",
     messagingSenderId: "638365950363"
   };
-  firebase.initializeApp(config);
 
-    // Create a variable to reference the database
-    var database = firebase.database();
+
+    firebase.initializeApp(config);
+
+    var dataRef = firebase.database();
 
     // Initial Values
-    var username = "";
-    var password = "";
-    //var rememberMe = "";
-    //var cancel = "";
-    //var forgetPwd = "";
+    var name = "";
+    var email = "";
+    var comment = "";
+
+    // Capture Button Click
+    $("#add-user").on("click", function(event) {
+      event.preventDefault();
+
+      // Code in the logic for storing and retrieving the most recent user.
+      // Don't forget to provide initial data to your Firebase database.
+      name = $("#name-input").val().trim();
+      email = $("#email-input").val().trim();
+      comment = $("#comment-input").val().trim();
+
+      // Code for the push
+      dataRef.ref().push({
+
+        name: name,
+        email: email,
+        comment: comment,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
+    });
+
+    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+    dataRef.ref().on("child_added", function(childSnapshot) {
+
+      // Log everything that's coming out of snapshot
+      console.log(childSnapshot.val().name);
+      console.log(childSnapshot.val().name);
+      console.log(childSnapshot.val().email);
+      console.log(childSnapshot.val().age);
+      console.log(childSnapshot.val().comment);
+      console.log(childSnapshot.val().joinDate);
+
+      // full list of items to the well
+      $("#full-member-list").append("<div class='well'><span id='name'> " + childSnapshot.val().name +
+        " </span><span id='email'> " + childSnapshot.val().email +
+        " </span><span id='age'> " + childSnapshot.val().age +
+        " </span><span id='comment'> " + childSnapshot.val().comment + " </span></div>");
+
+    // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+
+    dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+
+      // Change the HTML to reflect
+      $("#name-display").html(snapshot.val().name);
+      $("#email-display").html(snapshot.val().email);
+      $("#age-display").html(snapshot.val().age);
+      $("#comment-display").html(snapshot.val().comment);
+    });
+
+
+    // Code for the push
+    /*
+    dataRef.ref().push({
+
+      name: name,
+      email: email,
+      comment: comment,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+  });
+// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+    dataRef.ref().on("child_added", function(childSnapshot) {
+
+      // Log everything that's coming out of snapshot
+      console.log(childSnapshot.val().name);
+      console.log(childSnapshot.val().email);
+      console.log(childSnapshot.val().comment);
+      console.log(childSnapshot.val().joinDate);
+
+      // full list of items to the well
+      $("#full-member-list").append("<div class='well'><span id='name'> " + childSnapshot.val().name +
+        " </span><span id='email'> " + childSnapshot.val().email +
+        " </span><span id='comment'> " + childSnapshot.val().comment + " </span></div>");
+
+    // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+
+    dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+
+      // Change the HTML to reflect
+      $("#name-display").html(snapshot.val().name);
+      $("#email-display").html(snapshot.val().email);
+      $("#comment-display").html(snapshot.val().comment);
+    });    
+*/
+
+
+
+
+
+
+
+
 
 
 
 
     // Capture Button Click
-    $("#add-user").on("click", function(event) {
-      // Don't refresh the page!
-      event.preventDefault();
+    //$("#add-user").on("click", function(event) {
+    //  event.preventDefault();
 
       // YOUR TASK!!!
       // Code in the logic for storing and retrieving the most recent user.
       // Don't forget to provide initial data to your Firebase database.
-      username = $("#username-input").val().trim();
-      password = $("#password-input").val().trim();
-      //comment = $("#comment-input").val().trim();
+    //  name = $("#name-input").val().trim();
+    //  email = $("#email-input").val().trim();
+     // comment = $("#comment-input").val().trim();
 
-      database.ref().set({
-        username: username,
-        password: password,
-        //comment: comment
-      });
+      // Code for the push
+      //dataRef.ref().push({
 
-    });
+      //  name: name,
+      //  email: email,
+      //  comment: comment,
+      //  dateAdded: firebase.database.ServerValue.TIMESTAMP
+     // });
+   // });
 
-    // Firebase watcher + initial loader HINT: .on("value")
-    database.ref().on("value", function(snapshot) {
+    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+    //dataRef.ref().on("child_added", function(childSnapshot) {
 
-      var db = snapshot.val();
+      // Log everything that's coming out of snapshot
+      //console.log(childSnapshot.val().name);
+     // console.log(childSnapshot.val().name);
+     // console.log(childSnapshot.val().email);
+     // console.log(childSnapshot.val().comment);
+     // console.log(childSnapshot.val().joinDate);
 
+      // full list of items to the well
+      //$("#full-member-list").append("<div class='well'><span id='name'> " + childSnapshot.val().name +
+      //  " </span><span id='email'> " + childSnapshot.val().email +
+      //  " </span><span id='comment'> " + childSnapshot.val().comment + " </span></div>");
+
+    // Handle the errors
+   // }, function(errorObject) {
+   //   console.log("Errors handled: " + errorObject.code);
+   // });
+
+  //  dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
 
       // Change the HTML to reflect
-      $("#username-display").html(db.username);
-      $("#password-display").html(db.password);
-      //$("#comment-display").html(db.comment);
-
-      // Handle the errors
-    }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    });
-
+   //   $("#name-display").html(snapshot.val().name);
+    //  $("#email-display").html(snapshot.val().email);
+    //  $("#comment-display").html(snapshot.val().comment);
+   // });
